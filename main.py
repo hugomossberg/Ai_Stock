716f5be46a4a18de815837599703e03b0ef9f94b
Hugo Mossberg <Hugo.mossberg1@live.com>
2025-02-18 16:17:25 +0100

    dk

--- main.py
+++ main.py
@@ -2,12 +2,31 @@ import json
 import os
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
 import yfinance as yf
 from chatgpt_client import chat_gpt
+from ib_insync import *
 
+ib = IB()
+ib.connect("127.0.0.1", 4002, clientId=1)
+print("ansliten till ib")
+
+print(ib.accountValues())
+
+contract = Stock("NVDA", "SMART", "USD")
+ib.qualifyContracts(contract)
+ticker = ib.reqMktData(contract)
+ib.sleep(2)  # Vänta lite så att data hinner laddas
+print(ticker.last)
+
+ib.disconnect()
 load_dotenv()
-
 
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
@@ -20,27 +39,18 @@ async def chat_response(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
     await update.message.reply_text(response)  # Skicka tillbaka svaret till chatten
 
 
-
 async def user_ask_stock(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_message = update.message.text
     ai_respons = chat_gpt(f"Ge mig en analys i JSON-format: {user_message}")
-    
-
-    
 
 
 def main():
- 
 
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-
-
-
-    
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat_response))
     app.run_polling()
 
+
 if __name__ == "__main__":
     main()
-
