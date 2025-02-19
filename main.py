ee45a4f3d133bc42bbd6d96af47bf2348163bc69
Hugo Mossberg <Hugo.mossberg1@live.com>
2025-02-19 21:51:07 +0100

    kdkd

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ from telegram.ext import (
 )
 import nest_asyncio
 
+load_dotenv()
+
 nest_asyncio.apply()
 
 from chatgpt_client import chat_gpt
 from ibkr_client import IbClient
 
-load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 ib_client = None
@@ -27,6 +28,20 @@ async def chat_response(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
     user_message = update.message.text
     response = chat_gpt(user_message)
     await update.message.reply_text(response)
+
+
+async def ask_ai_stock(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Fråga om aktiens data via IBKR API."""
+    user_message = update.message.text  # Hämta texten från användarens meddelande
+    if user_message:
+        await update.message.reply_text(f"Hämtar aktier med ticker {user_message}...")
+        tickers = await ib_client.get_stocks([user_message])
+        if tickers:
+            await update.message.reply_text(
+                f"Hämtade {len(tickers)} aktier: {', '.join(tickers)}"
+            )
+        else:
+            await update.message.reply_text("Inga aktier hittades.")
 
 
 async def disconnect_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -47,6 +62,9 @@ async def main():
 
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat_response))
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, ask_ai_stock)
+    )  # fråga om aktier till ai
     app.add_handler(CommandHandler("dc", disconnect_command))
 
     await app.run_polling()  # Startar Telegram-boten
