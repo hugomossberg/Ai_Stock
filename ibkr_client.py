4db54e0dbee3dae9b157b6b00d867d5813eaf03d
Hugo Mossberg <Hugo.mossberg1@live.com>
2025-02-19 14:18:05 +0100

    jdjd

--- ibkr_client.py
+++ ibkr_client.py
@@ -3,23 +3,22 @@ from dotenv import load_dotenv
 from ib_insync import *
 import time
 import yfinance as yf
+import pandas as pd
 
 load_dotenv()
-
-
-sp500 = yf.Ticker("QQQ").holdings
-stocks = sp500.history(period="1d")
 
 
 class IbClient:
     def __init__(self):
         self.ib = IB()
         self.ib.connect("127.0.0.1", 4002, clientId=1)
+        self.df = pd.read_csv("S&P500.csv")
 
         print("api Connceted!")
 
-    def get_stock():
-        stocks.get_tickers()
+    def get_stocks(self):
+        self.tickers = self.df["Symbol"].tolist()
+        return self.tickers
 
     def disconnect_ibkr(self):
         time.sleep(2)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import os
+import sys
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import (
@@ -10,39 +11,51 @@ from telegram.ext import (
     MessageHandler,
     filters,
 )
-
 from chatgpt_client import chat_gpt
 from ibkr_client import IbClient
 
-
 load_dotenv()
 
-
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+# Skapa en global variabel för IBKR API-klienten
+ib_client = None
 
 
 async def chat_response(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Svara på alla meddelanden med ChatGPT"""
-    user_message = update.message.text  # Hämta användarens textmeddelande
-    response = chat_gpt(user_message)  # Skicka till OpenAI API
-    await update.message.reply_text(response)  # Skicka tillbaka svaret till chatten
+    user_message = update.message.text
+    response = chat_gpt(user_message)
+    await update.message.reply_text(response)
+
+
+async def disconnect_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Koppla från IBKR API via Telegram"""
+    if ib_client:
+        ib_client.disconnect_ibkr()
+        await update.message.reply_text("IBKR API disconnected!")
+    else:
+        await update.message.reply_text("IBKR API är redan nedkopplad.")
 
 
 def main():
-    # telegram
-    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+    global ib_client
 
-    # IBKR
+    # Starta IBKR
     ib_client = IbClient()
-    ib_client.get_stock()
+    tickers = ib_client.get_stocks()
+    print(tickers)
 
-    # openapi
+    # Starta Telegram
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat_response))
+    app.add_handler(CommandHandler("dc", disconnect_command))
 
-    # disconnect IBKR API
-    ib_client.disconnect_ibkr()
-
-    app.run_polling()
+    try:
+        app.run_polling()
+    finally:
+        # Disconnect IBKR API om programmet stängs
+        ib_client.disconnect_ibkr()
 
 
 if __name__ == "__main__":
